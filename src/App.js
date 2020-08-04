1d5c2889e3feba2b588e7c5393aa520b6c455d0e
Jeremiah Trnka <jeremiah@jeremiahs-mbp.lan>
2020-08-04 15:11:14 -0700

    wip

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import axios from 'axios'
+import Loading from './components/Loading'
+import Card from './components/Card'
 import './App.css';
 
 class App extends React.Component {
   constructor() {
     super()
     this.state = {
-      data: []
+      data: [],
+      loading: true
     }
   }
 
@@ -16,7 +19,8 @@ class App extends React.Component {
       .then( res => {
         console.log(res)
         this.setState({
-          data: res.data
+          data: res.data,
+          loading: !this.state.loading
         })
       })
 
@@ -30,17 +34,9 @@ render() {
   return (
     <div className="App container">
       <div className='cards'>
-      <div className='card'>
-      <img src={this.state.data.avatar_url} />
-      <div className='card-info'>
-        <h3 className='name'>{this.state.data.name}</h3>
-        <p className='username'>GitHub: <a href={this.state.data.html_url}>{this.state.data.login}</a></p>
-        <p>{`Location: ${this.state.data.location}`}</p>
-        <p>{`Followers: ${this.state.data.followers}`}</p>
-        <p>{`Following: ${this.state.data.following}`}</p>
-        <p>{`Bio: ${this.state.data.bio}`}</p>
-      </div>
-      </div>
+        {
+          this.state.loading ? <Loading /> : <Card data={this.state.data}/>
+        }
       </div>
     </div>
   )
